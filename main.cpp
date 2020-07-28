d94b4e79fddc8b9c463e4a1d10aa91c4c0f82a3f
Santiago <santiagoperaza@icloud.com>
2020-07-28 16:59:59 -0300

    finalizado

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ class persona{
         string apellido;
         // defino variables de persona
         string direccion;
-        int telefono;
+        string telefono;
     public:
     
     void set(){
@@ -67,14 +67,21 @@ class clientes: public persona{
 class empleados: public persona{
 
     public:
-        int codigoEmpleado;
+        int codigoEmpleado=0;
         string tipoEmpleado;
     
     public:
-        void set(){
-        cout << "Cedula cliente: ";
-        cin >> cedula;
-        cout << "Nombre cliente: ";
+        void setCodigo(int cod){
+        codigoEmpleado=cod;}
+        void setCedula(int ced){
+        cedula=ced;}
+        void setTipo(int tipo){
+        tipoEmpleado=tipo;}
+
+        
+        void setEmpleado(){
+        
+        cout << "Nombre empleado: ";
         cin >> nombre;
         cout << "Apellido: ";
         cin >> apellido;
@@ -82,10 +89,17 @@ class empleados: public persona{
         cin >> direccion;
         cout << "Telefono: ";
         cin >> telefono;
-        cout << "Codigo empleado: ";
-        cin >> codigoEmpleado;
         cout << "Tipo empleado: ";
         cin >> tipoEmpleado; 
+        }
+        void view(){
+            cout<< "Nombre: "<<nombre<<endl;
+            cout<< "Apellido: "<<apellido<<endl;
+            cout<< "Direccion: "<<direccion<<endl;
+            cout<< "Telefono: "<<telefono<<endl;
+            cout<< "Codigo empleado: "<<codigoEmpleado<<endl;
+            cout<< "Tipo empleado: "<<tipoEmpleado<<endl;
+
         }
 };
 class cuenta{
@@ -98,9 +112,10 @@ class cuenta{
     float sobregiro=0;
 
     public:
+    void setNumero(int num){
+        numeroDeCuenta=num;}
     void set(){
-        cout << "Número de cuenta: ";
-        cin >> numeroDeCuenta;
+        
         cout << "Tipo(CA/CC): ";
         cin >> tipo;
         
@@ -189,26 +204,48 @@ class registros {
 
 };
 
+
+class historial{
+    public:
+    int empleado=-1;
+    int cuenta =-1;
+    string transaccion;
+   
+    
+    void set(int emp,int cuen,string trans){
+        empleado = emp;
+        cuenta = cuen;
+        transaccion = trans;
+        
+    }
+    void view(){
+        cout<<"Empleado: "<<empleado<<", Cuenta: "<<cuenta<<", transacción: "<<transaccion<<endl;
+    }
+  
+};
+
 int main(){
     int opcion =-1;
     clientes clientesLista[20];
     empleados empleadosLista[20];
     
-
+    historial transacciones[50];
     registros registroLista[20];
   
 
      while (opcion != 0)
     {
         cout << "Ingrese la opción deseada. " << endl;
-        cout << "\t1-Ingresar cliente " << endl;
-        cout << "\t2-Listar clientes. " << endl;
-        cout << "\t3-Crear cuenta. " << endl;
-        cout << "\t4-Listar cuenta. " << endl;
-        cout << "\t5-Depositar saldo. " << endl;
-        cout << "\t6-Retirar. " << endl;
-        cout << "\t7-Saldo total bancario. " << endl;
-        // cout << "\t8-Retirar. " << endl;
+        cout << "\t1-Ingresar empleado " << endl;
+        cout << "\t2-Ingresar cliente " << endl;
+        cout << "\t3-Listar empleados. " << endl;
+        cout << "\t4-Listar clientes. " << endl;
+        cout << "\t5-Crear cuenta. " << endl;
+        cout << "\t6-Listar cuenta. " << endl;
+        cout << "\t7-Depositar saldo. " << endl;
+        cout << "\t8-Retirar. " << endl;
+        cout << "\t9-Saldo total bancario. " << endl;
+        cout << "\t10-Historial de transacciones. " << endl;
         
         cout << "\t0-Salir " << endl;
         cin>> opcion;
@@ -216,6 +253,40 @@ int main(){
         {
             // Crea usuario
         case 1:{
+        
+            cout<<""<<endl;
+            int posicion=0;
+            int codigo =1;
+            int cedula =0;
+            int duplicado =0;
+            cout << "Cedula empleado: ";
+            cin >> cedula;
+            while(empleadosLista[posicion].codigoEmpleado!=0){
+                if (empleadosLista[posicion].cedula ==cedula)
+                {   cout<<"Usario ya registrado"<<endl;
+                    duplicado = -1;
+                    break;
+                }else{
+                posicion++;
+                codigo++;
+                }
+                
+                
+            }if (duplicado !=-1)
+            {
+            empleadosLista[posicion].setCedula(cedula);
+            empleadosLista[posicion].setCodigo(codigo);
+            empleadosLista[posicion].setEmpleado();
+            
+            cout<<"================================"<<endl;  /* code */
+            }
+            
+           
+            break;
+            }
+
+        case 2:{
+
             cout<<""<<endl;
             int posicion=0;
             int codigo =1;
@@ -244,9 +315,25 @@ int main(){
             
            
             break;
-            }
+        }
+
+
             // Lista usuarios
-        case 2:{
+        case 3:{
+            cout<<""<<endl;
+            int posicion = 0;
+            cout<<"Empleado ||  Apellido"<<endl;
+            while(empleadosLista[posicion].codigoEmpleado!=0){
+                cout<<empleadosLista[posicion].codigoEmpleado;
+                cout<< "        ||    ";
+                cout<< empleadosLista[posicion].apellido<<endl;
+                posicion++;
+            }
+            
+            cout<<"================================"<<endl;  
+            break;
+        }
+         case 4:{
             cout<<""<<endl;
             int posicion = 0;
             cout<<"Cliente ||  Apellido"<<endl;
@@ -261,10 +348,16 @@ int main(){
             break;
         }
             // Crea cuenta
-        case 3:{
+        case 5:{
             int cliente = -1;
             // int lista[20];
             cout<<""<<endl;
+
+            if (clientesLista[0].codigoCliente==0)
+            {
+                cout<< "No existen clientes registrados"<<endl;
+                break;
+            }
             cout<<"Seleccione un cliente de la lista:"<<endl;
 
             int posicion = 0;
@@ -275,7 +368,10 @@ int main(){
                 cout<< clientesLista[posicion].apellido<<endl;
                 posicion++;
             }
+            
+            
             cuenta ingresar;
+            int numeroCuenta=100;
             cin>>cliente;
             posicion=0;
             int registroPos=0;
@@ -286,9 +382,11 @@ int main(){
                      while (registroLista[registroPos].status!=-1)
                      {
                          registroPos++;
+                         numeroCuenta++;
                      }
 
-                     registroLista[registroPos].cliente.codigoCliente=cliente;
+                     registroLista[registroPos].cliente.codigoCliente=cliente;\
+                     ingresar.setNumero(numeroCuenta);
                      ingresar.set();
                      registroLista[registroPos].cuentaCliente=ingresar;
                      registroLista[registroPos].status=1;
@@ -313,7 +411,7 @@ int main(){
             break;
             }
             // Lista cuentas
-        case 4:{
+        case 6:{
             int cuentaPos=0;
             while(registroLista[cuentaPos].status!=-1){
                 cout<<"Cuenta: "<<registroLista[cuentaPos].cuentaCliente.numeroDeCuenta<<endl;
@@ -322,16 +420,43 @@ int main(){
             break;
         }
             // Deposita
-        case 5:{
-            
+        case 7:{
+            if ((registroLista[0].status==-1 ))
+            {
+                cout<<"No existe cuenta registrada"<<endl;
+                break;
+            }else if( empleadosLista[0].codigoEmpleado==0){
+                cout<<"No existe empleado registrado"<<endl;
+                break;
+            }
+            int empleado =0;
             int cuentaPos=0;
             int cuenta=-1;
-            cout<< "Seleccione una cuenta de la lista:"<<endl;
+            int empleadoPos = 0;
+            cout<< "Seleccione un empleado:"<<endl;
+            while(empleadosLista[empleadoPos].codigoEmpleado!=0){
+                cout<<"Codigo de empleado: "<<empleadosLista[empleadoPos].codigoEmpleado<<" - Apellido - "<<empleadosLista[empleadoPos].apellido<<"Tipo -"<<empleadosLista[empleadoPos].tipoEmpleado<<endl;
+                empleadoPos++;
+              }
+              
+              cin>>empleado;
+              if(empleado<1||empleado>empleadoPos){
+                  cout<<"empleado incorrecto"<<endl;
+                  break;
+              }
+
+
+            cout<< "Seleccione una cuenta:"<<endl;
             while(registroLista[cuentaPos].status!=-1){
                 cout<<"Cuenta: "<<registroLista[cuentaPos].cuentaCliente.numeroDeCuenta<<endl;
                 cuentaPos++;
               }
             cin>>cuenta;
+            
+            if(cuenta<100||cuenta>registroLista[cuentaPos-1].cuentaCliente.numeroDeCuenta){
+                  cout<<"cuenta incorrecta"<<endl;
+                  break;
+              }
             cuentaPos=0;
             while(registroLista[cuentaPos].cuentaCliente.numeroDeCuenta!=cuenta){
                  cuentaPos++;
@@ -340,29 +465,71 @@ int main(){
             // cout<<cuentaPos<<endl;
             registroLista[cuentaPos].cuentaCliente.deposit();
             registroLista[cuentaPos].cuentaCliente.view();
+            int transIndex=0;
+            while (transacciones[transIndex].cuenta!=-1)
+            {
+              transIndex++;
+              
+            }
+            historial reg;
+            reg.set(empleado,cuenta,"Deposito");
+            
+            transacciones[transIndex]=reg;
+            
             break;
         }
             // Retirar
-        case 6:{
+        case 8:{
             // Retirar
             int cuentaPos=0;
             int cuenta=-1;
+            int empleado=0;
+            int empleadoPos = 0;
+            cout<< "Seleccione un empleado:"<<endl;
+            while(empleadosLista[empleadoPos].codigoEmpleado!=0){
+                cout<<"Codigo de empleado: "<<empleadosLista[empleadoPos].codigoEmpleado<<" - Apellido - "<<empleadosLista[empleadoPos].apellido<<"Tipo -"<<empleadosLista[empleadoPos].tipoEmpleado<<endl;
+                empleadoPos++;
+              }
+              
+              cin>>empleado;
+              if(empleado<1||empleado>empleadoPos){
+                  cout<<"empleado incorrecto"<<endl;
+                  break;
+              }
+
             cout<< "Seleccione una cuenta de la lista:"<<endl;
             while(registroLista[cuentaPos].status!=-1){
                 cout<<"Cuenta: "<<registroLista[cuentaPos].cuentaCliente.numeroDeCuenta<<endl;
                 cuentaPos++;
               }
             cin>>cuenta;
+            if(cuenta<100||cuenta>registroLista[cuentaPos-1].cuentaCliente.numeroDeCuenta){
+                  cout<<"cuenta incorrecta"<<endl;
+                  break;
+              }
             cuentaPos=0;
+
             while(registroLista[cuentaPos].cuentaCliente.numeroDeCuenta!=cuenta){
                  cuentaPos++;
             }
             registroLista[cuentaPos].cuentaCliente.retirement();
             registroLista[cuentaPos].cuentaCliente.view();
+            int transIndex=0;
+            while (transacciones[transIndex].cuenta!=-1)
+            {
+              transIndex++;
+              
+            }
+            historial reg;
+            reg.set(empleado,cuenta,"Retiro");
+            
+            transacciones[transIndex]=reg;
+            
+            
             break;
         }
             // Saldo total bancario
-        case 7:{
+        case 9:{
             int cuentaPos=0;
             float saldoBancarioPeso= 0;
             float saldoBancarioDolares= 0;
@@ -396,6 +563,18 @@ int main(){
 
 
        
+            }
+            // Historial de transacciones
+        case 10:{
+          
+            int transIndex = 0;
+            while (transacciones[transIndex].cuenta!=-1)
+            {
+                transacciones[transIndex].view();
+                transIndex++;
+              
+            }
+            
             }
         }   
     }
