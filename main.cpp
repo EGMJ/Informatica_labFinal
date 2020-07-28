23db6a7946a93af17ba1f03baf78b331611fa0e3
Santiago <santiagoperaza@icloud.com>
2020-07-27 23:56:47 -0300

    diversas implementaciones

--- main.cpp
+++ main.cpp
@@ -95,7 +95,7 @@ class cuenta{
     string tipo;    
     string moneda;
     float saldo =0;
-    float sobregiro;
+    float sobregiro=0;
 
     public:
     void set(){
@@ -135,22 +135,48 @@ class cuenta{
         cout<< "Sobregiro: "<<sobregiro <<endl;
         cout<< "=============================="<<endl;
     }  
-    void deposit(float monto){
+    void deposit(){
+        float monto =0;
+        cout<<"Ingrese el monto a depositar: ";
+        cin>>monto;
+        if (monto>0)
+        {
         saldo = saldo+monto;
-        cout<< "Saldo actual: "<< saldo<<endl;
+        cout<< "Saldo actual: "<< saldo<<endl;  /* code */
+        }else
+        {
+            cout<<"Monto incorrecto"<<endl;
+        }
+        
+        
+        
     }     
-    void retirement(float monto){
+    void retirement(){
+        float monto =0;
+        cout<<"Ingrese el monto a retirar: ";
+        cin>>monto;
+        if (monto>0 && saldo>0)
+        {
+            /* code */ 
         if(tipo == "CC"){
-        if(monto<(saldo+(sobregiro*saldo)/100)){
+        if(monto<=(saldo+(sobregiro*saldo)/100)){
         saldo = saldo-monto;
         }else{
             cout<< "Saldo insuficiente"<< endl;
         }
-    }else{
+        }else{
         if (monto<= saldo){
             saldo = saldo-monto;
             }
         }
+        }else{
+            cout<<"Monto incorrecto o saldo insuficiente"<<endl;
+        }
+    }
+    float _saldo()
+    {return saldo;}
+    string _moneda(){
+        return moneda;
     }
 };
 
@@ -167,7 +193,7 @@ int main(){
     int opcion =-1;
     clientes clientesLista[20];
     empleados empleadosLista[20];
-    cuenta cuentaLista[20];
+    // cuenta cuentaLista[20];
     // cuenta1.set();
     // cuenta1.view();
 
@@ -183,8 +209,8 @@ int main(){
         cout << "\t4-Listar cuenta. " << endl;
         cout << "\t5-Depositar saldo. " << endl;
         cout << "\t6-Retirar. " << endl;
-        cout << "\t7-Salto total bancario. " << endl;
-        cout << "\t8-Retirar. " << endl;
+        cout << "\t7-Saldo total bancario. " << endl;
+        // cout << "\t8-Retirar. " << endl;
         
         cout << "\t0-Salir " << endl;
         cin>> opcion;
@@ -232,7 +258,7 @@ int main(){
                 cout<< clientesLista[posicion].apellido<<endl;
                 posicion++;
             }
-            
+            cuenta ingresar;
             cin>>cliente;
             posicion=0;
             int registroPos=0;
@@ -244,10 +270,13 @@ int main(){
                      {
                          registroPos++;
                      }
+
                      registroLista[registroPos].cliente.codigoCliente=cliente;
-                     registroLista[registroPos].cuentaCliente.set();
+                     ingresar.set();
+                     registroLista[registroPos].cuentaCliente=ingresar;
                      registroLista[registroPos].status=1;
                      cout<<"Cuenta creada con exito"<<endl;
+                     registroLista[registroPos].cuentaCliente.view();
                      cout<<"================================"<<endl;
                      break;
                  }else{
@@ -255,8 +284,13 @@ int main(){
                  }
                  posicion++;
             
+            } 
+            if (registroLista[registroPos].status!=1)
+            {
+                cout<<"Cliente ingresado incorrecto"<<endl;/* code */
             }
-            cout<<"Cliente ingresado incorrecto"<<endl;
+            
+            
             
             break;
             }
@@ -267,20 +301,84 @@ int main(){
                 cout<<"Cuenta: "<<registroLista[cuentaPos].cuentaCliente.numeroDeCuenta<<endl;
                 cuentaPos++;
               }
+            break;
         }
         case 5:{
+            
             int cuentaPos=0;
+            int cuenta=-1;
+            cout<< "Seleccione una cuenta de la lista:"<<endl;
             while(registroLista[cuentaPos].status!=-1){
                 cout<<"Cuenta: "<<registroLista[cuentaPos].cuentaCliente.numeroDeCuenta<<endl;
                 cuentaPos++;
               }
+            cin>>cuenta;
+            cuentaPos=0;
+            while(registroLista[cuentaPos].cuentaCliente.numeroDeCuenta!=cuenta){
+                 cuentaPos++;
+            }
+            
+            cout<<cuentaPos<<endl;
+            registroLista[cuentaPos].cuentaCliente.deposit();
+            registroLista[cuentaPos].cuentaCliente.view();
+            break;
         }
-        case 6:{}
-        case 7:{}
-        case 8:{}
+        case 6:{
+            // Retirar
+            int cuentaPos=0;
+            int cuenta=-1;
+            cout<< "Seleccione una cuenta de la lista:"<<endl;
+            while(registroLista[cuentaPos].status!=-1){
+                cout<<"Cuenta: "<<registroLista[cuentaPos].cuentaCliente.numeroDeCuenta<<endl;
+                cuentaPos++;
+              }
+            cin>>cuenta;
+            cuentaPos=0;
+            while(registroLista[cuentaPos].cuentaCliente.numeroDeCuenta!=cuenta){
+                 cuentaPos++;
+            }
+            registroLista[cuentaPos].cuentaCliente.retirement();
+            registroLista[cuentaPos].cuentaCliente.view();
+            break;
         }
-    }
+        case 7:{
+            int cuentaPos=0;
+            float saldoBancarioPeso= 0;
+            float saldoBancarioDolares= 0;
 
+            
+            while(registroLista[cuentaPos].status!=-1){
+                
+                string moneda = registroLista[cuentaPos].cuentaCliente._moneda();
+                float saldo = registroLista[cuentaPos].cuentaCliente._saldo();
+            
+                if (moneda=="$" && saldo>0 )
+                    {
+                    saldoBancarioPeso = saldoBancarioPeso + saldo;
+                    cuentaPos++;
+                    }else if  (moneda=="U$S"&& saldo>0)
+                    {
+                    saldoBancarioDolares = saldoBancarioPeso + saldo;
+                    cuentaPos++;
+                    }else{
+                        cuentaPos++;
+                    }
+                
+                
+                }
+            cout<<"================================"<<endl;
+            cout<< "Saldo en Pesos: "<<saldoBancarioPeso<<endl;
+            cout<<"Saldo en Dolares: "<<saldoBancarioDolares<<endl;
+            cout<<"================================"<<endl;
+
+            break;
+
+
+       
+            }
+        }   
+    }
     return 0;
 
+    
 }
